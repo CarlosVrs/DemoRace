1cb0a73b5aa7934b0941b3216900af4e0d96207a
Carlos Orozco <carlosorozco41@gmail.com>
2022-10-09 20:57:03 -0500

    Unified wheel logic

--- DemoRaceProject/Assets/src/Car/CarController.cs
+++ DemoRaceProject/Assets/src/Car/CarController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CarController : MonoBehaviour
 {
@@ -16,7 +17,9 @@ public class CarController : MonoBehaviour
     public float torque;
     public float brakeForze;
     public AnimationCurve torqueCurve;
-    public AnimationCurve brakeCurve;    
+    public AnimationCurve brakeCurve;
+
+    private float carSpeed;    
 
     [Header("Wheels")]
     public GameObject wheelModel;
@@ -27,13 +30,16 @@ public class CarController : MonoBehaviour
     [Range(-1, 1)]
     public float accelInput;
     
-    private GameObject[] wheels;
+    private WheelController[] wheels;
+    private RaycastHit raycastHit;
+    private bool rayDidHit;
+    private float offset;
 
     
     // Start is called before the first frame update
     void Start()
     {
-        wheels = new GameObject[4];
+        wheels = new WheelController[4];
         
         if(wheelModel != null){
             wheels[0] = wheelSetup(WheelController.WheelPos.FRONT_LEFT);
@@ -43,10 +49,10 @@ public class CarController : MonoBehaviour
         }
 
         rb.mass = mass;
-        rb.collisionDetectionMode = detectionMode; 
+        rb.collisionDetectionMode = detectionMode;
     }
 
-    GameObject wheelSetup(WheelController.WheelPos pos){
+    WheelController wheelSetup(WheelController.WheelPos pos){
         GameObject newWheel = Instantiate(wheelModel, transform, false);
         WheelController wheelController = newWheel.GetComponent<WheelController>(); 
         switch (pos)
@@ -69,12 +75,112 @@ public class CarController : MonoBehaviour
                 break;
         }
 
-        return newWheel;
+        return wheelController;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void FixedUpdate(){
+        foreach (WheelController wheelC in wheels)
+        {
+            rayDidHit = Physics.Raycast(wheelC.transform.position, -wheelC.transform.up, out raycastHit, wheelC.springRestDistance);
+
+            if(rayDidHit){
+                //Suspension spring force
+                Vector3 springDir = wheelC.wheelTransform.up;
+                Vector3 tireWorldVel = rb.GetPointVelocity(wheelC.wheelTransform.position) / Time.fixedDeltaTime;
+                
+                offset = wheelC.springRestDistance - raycastHit.distance;
+
+                float vel = Vector3.Dot(springDir, tireWorldVel);
+
+                float force = (offset * wheelC.springStrength) - (vel * wheelC.springDamper);
+
+                rb.AddForceAtPosition(springDir * force, wheelC.wheelTransform.position);
+            
+                //Steering force
+                Vector3 steeringDir = wheelC.wheelTransform.right;
+                tireWorldVel = rb.GetPointVelocity(wheelC.wheelTransform.position);
+
+                float steeringVel = Vector3.Dot(steeringDir, tireWorldVel);
+                float desiredVelChange = -steeringVel * wheelC.wheelGripFactor;
+                float desiredAccel = desiredVelChange / Time.fixedDeltaTime;
+
+                rb.AddForceAtPosition(steeringDir * wheelC.wheelMass * desiredAccel, wheelC.wheelTransform.position);
+            
+                //Friction
+                if (!(carSpeed < 0.01 && -0.01 < carSpeed)){
+                    float desiredFrictionChange = Mathf.Sign(carSpeed) * -wheelC.frictionFactor * wheelC.friction;
+                    float desiredFrictionAccel = desiredFrictionChange / Time.fixedDeltaTime;
+
+                    rb.AddForceAtPosition(wheelC.wheelTransform.forward * rb.mass * desiredFrictionAccel, wheelC.wheelTransform.position);
+
+                }
+
+                //Acceleration 
+                Vector3 accelDir = wheelC.wheelTransform.forward;
+                carSpeed = Vector3.Dot(wheelC.transform.forward, rb.velocity);
+
+                if(accelInput > 0.0f && carSpeed < carTopSpeed){
+                    float normalizedSpeed = Mathf.Clamp01(Mathf.Abs(carSpeed) / carTopSpeed);
+                    float availableTorque = torqueCurve.Evaluate(normalizedSpeed) * accelInput;
+
+                    rb.AddForceAtPosition(accelDir * availableTorque * torque, wheelC.wheelTransform.position, ForceMode.Impulse);
+                    
+                }
+                
+                // Braking and reverse
+                if(accelInput < 0.0f && carSpeed > -carBackwardsTopSpeed){
+                    // Braking
+                    float normalizedSpeed = Mathf.Clamp01(Mathf.Abs(carSpeed) / carTopSpeed);
+                    float availableTorque = brakeCurve.Evaluate(normalizedSpeed) * accelInput;
+                    availableTorque *= brakeForze;     
+                    
+                    //Reverse
+                    if(carSpeed <= 0.0f){
+                        normalizedSpeed = Mathf.Clamp01(Mathf.Abs(carSpeed) / carBackwardsTopSpeed);
+                        availableTorque = torqueCurve.Evaluate(normalizedSpeed) * accelInput;
+                        availableTorque *= torque;
+
+                    }
+
+                    rb.AddForceAtPosition(accelDir * availableTorque, wheelC.wheelTransform.position, ForceMode.Impulse);
+                    
+                }
+            }
+
+            //Visual spin speed
+            wheelC.spinSpeedFrame = (wheelC.transform.position - wheelC.posInLastFrame).magnitude;
+            wheelC.posInLastFrame = wheelC.transform.position;
+        }
+    }
+
+    void Update(){
+        foreach (WheelController wheelC in wheels)
+        {
+            rayDidHit = Physics.Raycast(wheelC.transform.position, -wheelC.transform.up, out raycastHit, wheelC.springRestDistance);
+            //Visual suspension
+            if(rayDidHit){
+                wheelC.wheelVisual.transform.localPosition = new Vector3(
+                    wheelC.wheelVisual.transform.localPosition.x, 
+                    wheelC.wheelRadius - raycastHit.distance, 
+                    wheelC.wheelVisual.transform.localPosition.z
+                );
+
+            }else{
+                wheelC.wheelVisual.transform.localPosition = new Vector3(
+                    wheelC.wheelVisual.transform.localPosition.x, 
+                    wheelC.wheelRadius - wheelC.springRestDistance, 
+                    wheelC.wheelVisual.transform.localPosition.z
+                );
+
+            }
+
+            // Visual rotation
+            if(!(accelInput < 0 && carSpeed > 0)){
+                float direction = accelInput == 0 ? Mathf.Sign(carSpeed) : Mathf.Sign(accelInput);
+                float valueToRotate = wheelC.spinSpeedFrame / (2 * Mathf.PI * wheelC.wheelRadius) * direction;
+                wheelC.wheelVisual.Rotate(Vector3.right, valueToRotate * 360, Space.Self);
+
+            }
+        }
     }
 }
--- DemoRaceProject/Assets/src/Car/WheelController.cs
+++ DemoRaceProject/Assets/src/Car/WheelController.cs
@@ -18,8 +18,8 @@ public class WheelController : MonoBehaviour
     [Range(0, 1)]
     public float frictionFactor;
     public float wheelMass;
-
-    private float friction;
+    [HideInInspector]
+    public float friction;
     
     private float springMaxLength;
     public Transform wheelTransform;
@@ -31,9 +31,10 @@ public class WheelController : MonoBehaviour
     private RaycastHit raycastHit;
     private bool rayDidHit;
     private float carSpeed;
-
-    private float spinSpeedFrame;
-    private Vector3 posInLastFrame;
+    [HideInInspector]
+    public float spinSpeedFrame;
+    [HideInInspector]
+    public Vector3 posInLastFrame;
 
     public enum WheelPos{
         FRONT_LEFT,
@@ -52,110 +53,5 @@ public class WheelController : MonoBehaviour
         springMaxLength = springRestDistance + springTravel;
 
         friction = 0.01f;
-    }
-
-    void FixedUpdate(){
-        rayDidHit = Physics.Raycast(transform.position, -transform.up, out raycastHit, springMaxLength);
-        
-        if(rayDidHit){
-            //Suspension spring force
-            Vector3 springDir = wheelTransform.up;
-            Vector3 tireWorldVel = carRb.GetPointVelocity(wheelTransform.position) / Time.fixedDeltaTime;
-            
-            offset = springRestDistance - raycastHit.distance;
-
-            float vel = Vector3.Dot(springDir, tireWorldVel);
-
-            float force = (offset * springStrength) - (vel * springDamper);
-
-            carRb.AddForceAtPosition(springDir * force, wheelTransform.position);
-
-            // Debug.DrawLine(transform.position,  new Vector3(transform.position.x, offset, transform.position.z), Color.blue);
-            // Debug.DrawLine(new Vector3(transform.position.x, offset, transform.position.z), raycastHit.point, Color.green);
-        
-            //Steering force
-            Vector3 steeringDir = wheelTransform.right;
-            tireWorldVel = carRb.GetPointVelocity(wheelTransform.position);
-
-            float steeringVel = Vector3.Dot(steeringDir, tireWorldVel);
-            float desiredVelChange = -steeringVel * wheelGripFactor;
-            float desiredAccel = desiredVelChange / Time.fixedDeltaTime;
-
-            carRb.AddForceAtPosition(steeringDir * wheelMass * desiredAccel, wheelTransform.position);
-
-            //Acceleration 
-            Vector3 accelDir = wheelTransform.forward;
-            carSpeed = Vector3.Dot(transform.forward, carRb.velocity);
-
-            if(cc.accelInput > 0.0f && carSpeed < cc.carTopSpeed){
-                float normalizedSpeed = Mathf.Clamp01(Mathf.Abs(carSpeed) / cc.carTopSpeed);
-                float availableTorque = cc.torqueCurve.Evaluate(normalizedSpeed) * cc.accelInput;
-
-                carRb.AddForceAtPosition(accelDir * availableTorque * cc.torque, wheelTransform.position, ForceMode.Impulse);
-                
-            }
-            
-            // Braking and reverse
-            if(cc.accelInput < 0.0f && carSpeed > -cc.carBackwardsTopSpeed){
-                // Braking
-                float normalizedSpeed = Mathf.Clamp01(Mathf.Abs(carSpeed) / cc.carTopSpeed);
-                float availableTorque = cc.brakeCurve.Evaluate(normalizedSpeed) * cc.accelInput;
-                availableTorque *= cc.brakeForze;     
-                
-                //Reverse
-                if(carSpeed <= 0.0f){
-                    normalizedSpeed = Mathf.Clamp01(Mathf.Abs(carSpeed) / cc.carBackwardsTopSpeed);
-                    availableTorque = cc.torqueCurve.Evaluate(normalizedSpeed) * cc.accelInput;
-                    availableTorque *= cc.torque;
-
-                }
-
-                carRb.AddForceAtPosition(accelDir * availableTorque, wheelTransform.position, ForceMode.Impulse);
-                
-            }
-            
-            //Friction
-            if (!(carSpeed < 0.01 && -0.01 < carSpeed)){
-                float desiredFrictionChange = Mathf.Sign(carSpeed) * -frictionFactor * friction;
-                float desiredFrictionAccel = desiredFrictionChange / Time.fixedDeltaTime;
-
-                carRb.AddForceAtPosition(accelDir * carRb.mass * desiredFrictionAccel, wheelTransform.position);
-
-            }
-
-        }
-
-        //Visual spin speed
-        spinSpeedFrame = (transform.position - posInLastFrame).magnitude;
-        posInLastFrame = transform.position;
-    }
-
-    void Update(){
-
-        //Visual suspension
-        if(rayDidHit){
-            wheelVisual.transform.localPosition = new Vector3(
-                wheelVisual.transform.localPosition.x , 
-                wheelRadius - raycastHit.distance, 
-                wheelVisual.transform.localPosition.z
-            );
-
-        }else{
-            wheelVisual.transform.localPosition = new Vector3(
-                wheelVisual.transform.localPosition.x , 
-                wheelRadius - springMaxLength, 
-                wheelVisual.transform.localPosition.z
-            );
-
-        }
-
-        // Visual rotation
-        if(!(cc.accelInput < 0 && carSpeed > 0)){
-            float direction = cc.accelInput == 0 ? Mathf.Sign(carSpeed) : Mathf.Sign(cc.accelInput);
-            float valueToRotate = spinSpeedFrame / (2 * Mathf.PI * wheelRadius) * direction;
-            wheelVisual.Rotate(Vector3.right, valueToRotate * 360, Space.Self);
-
-        }
-        
     }
 }
